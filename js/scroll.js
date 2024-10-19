2f4f171058e7a28ad8a3c7a5a5ddfb840079b10b
VMJ <viktor.mihaly.jozsa@gmail.com>
2024-10-19 19:31:27 +0200

    Mobil and Tablet Rotation Stop

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,17 @@
+/*  ========================================================================  *\
+
+    V M J  ~  S C R O L L . J S
+
+    * C O M E N T
+    
+    This JavaScript feature may prevent the website from being rotated,
+    when viewing on a mobile or tablet.
+
+\*  ========================================================================  */
+
+
 window.addEventListener("orientationchange", function() {
     if (window.orientation === 90 || window.orientation === -90) {
         alert("Please turn the phone back to portrait view.");
     }
     }, false);
-
-/* window.scrollTo(0, document.body.scrollHeight);  /* A dokumentum aljara visz betolteskor */
